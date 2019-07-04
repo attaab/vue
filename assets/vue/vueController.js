949731eda88080344bfbd29eeeb57113db273d2a
codefingers <udohezekiel34@yahoo.com>
2019-07-04 17:38:42 +0100

    created 2 eay data binding feature

--- assets/vue/vueController.js
+++ assets/vue/vueController.js
@@ -1,63 +1,9 @@
 new Vue ({
     el : "#vue-app",
     data : {
-        website : "https://github.com/codefingas/vue"
+       name : ""
     },
-    computed: {
-        addAgeToA : function () {
-            return this.age + this.a;
-        },
-        addAgeToB : function () {
-            return this.age + this.b;
-        }
+    methods: {
+       
     }
 })
-
-/**this vue instance is for the keyboard events */
-// new Vue({
-//     el: "#vue-app",
-//     data : {
-//         name : "name",
-//         age : "age"
-//     },
-//     methods: {
-//         logName : function () {
-            
-//             console.log("you entered your name");
-//         },
-
-//         logAge : function () {
-//             console.log("you entered your age");
-//         }
-//     }
-
-
-// })
-
-
-
-
-/**this vue instance is for the  events lesson*/
-// new Vue({
-//     el: '#vue-app',
-//     data: {
-//         person : {
-//             name : "shaun"
-//     },
-//     age : 26,
-//         x : 0,
-//         y : 0
-//     },
-//     methods: {
-//         add : function (inc)    {
-//             this.age += inc;
-//         },
-//         subtract : function (dec) {
-//             this.age -= dec;
-//         },
-//         updateXY : function (event) {
-//            this.x = event.offsetX;
-//            this.y = event.offsetY;
-//         }
-//     }
-// });
