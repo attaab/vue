208a6f8dfd2172f649d95725fa7b00027967d89b
codefingers <udohezekiel34@yahoo.com>
2019-07-04 13:40:58 +0100

    added index file

---
+++ assets/vue/vueController.js
@@ -0,0 +1,65 @@
+new Vue ({
+    el : "#vue-app",
+    data : {
+        age : 20,
+        a : 0,
+        b : 0
+    },
+    computed: {
+        addAgeToA : function () {
+            return this.age + this.a;
+        },
+        addAgeToB : function () {
+            return this.age + this.b;
+        }
+    }
+})
+
+/**this vue instance is for the keyboard events */
+// new Vue({
+//     el: "#vue-app",
+//     data : {
+//         name : "name",
+//         age : "age"
+//     },
+//     methods: {
+//         logName : function () {
+            
+//             console.log("you entered your name");
+//         },
+
+//         logAge : function () {
+//             console.log("you entered your age");
+//         }
+//     }
+
+
+// })
+
+
+
+
+/**this vue instance is for the  events lesson*/
+// new Vue({
+//     el: '#vue-app',
+//     data: {
+//         person : {
+//             name : "shaun"
+//     },
+//     age : 26,
+//         x : 0,
+//         y : 0
+//     },
+//     methods: {
+//         add : function (inc)    {
+//             this.age += inc;
+//         },
+//         subtract : function (dec) {
+//             this.age -= dec;
+//         },
+//         updateXY : function (event) {
+//            this.x = event.offsetX;
+//            this.y = event.offsetY;
+//         }
+//     }
+// });
