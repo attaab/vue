5122c14383fd849d1185bef1c1495ea6698926ef
codefingers <udohezekiel34@yahoo.com>
2019-07-04 14:00:56 +0100

    added webstie link

--- assets/vue/vueController.js
+++ assets/vue/vueController.js
@@ -1,9 +1,7 @@
 new Vue ({
     el : "#vue-app",
     data : {
-        age : 20,
-        a : 0,
-        b : 0
+        website : "https://github.com/codefingas"
     },
     computed: {
         addAgeToA : function () {
