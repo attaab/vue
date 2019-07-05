16087adf2f6269ec96577f70f63139a91479709f
codefingers <udohezekiel34@yahoo.com>
2019-07-05 16:54:13 +0100

    finished the synamic css feature

--- assets/vue/vueController.js
+++ assets/vue/vueController.js
@@ -1,18 +1,15 @@
 new Vue ({
     el : "#vue-app",
     data : {
-       age : 20,
-       a : 0,
-       b : 0
+       available : false,
+       nearBy : false,
     },
-    computed: {
-        addtoA : function () {
-            console.log("addtoA");
-           return this.a + this.age;
-        },
-        addToB : function () {
-            console.log("addToB");
-            return this.b + this.age;
+    computed : {
+        compClasses : function () {
+            return {
+                available : this.available,
+                nearby : this.nearBy
+            }
         }
     }
 })
