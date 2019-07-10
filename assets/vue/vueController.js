80b76680a7029eaa7d1214066d2b6be4254e64dd
codefingers <udohezekiel34@yahoo.com>
2019-07-10 17:42:32 +0100

    finished the punch bag game

--- assets/vue/vueController.js
+++ assets/vue/vueController.js
@@ -1,65 +1,26 @@
 new Vue ({
     el : "#vue-app",
     data : {
-        age : 20,
-        a : 0,
-        b : 0
+        width: 100,
+        ended: false
+    },
+    methods: {
+        punch : function () {
+            this.width -= 10;//this reduces the width of the bag health
+                            //by 10 everytime the function is fired
+
+            if (this.width <= 0) {//this changes the boolean value when the width has been completely reduced to zero
+                this.ended = true;
+            }
+
+        },
+        restart : function () {//this function restarts the game by changing the ended boolean value to true
+            this.ended = false;
+            this.width = 100;
+        }
     },
     computed: {
-        addAgeToA : function () {
-            return this.age + this.a;
-        },
-        addAgeToB : function () {
-            return this.age + this.b;
-        }
+
+        
     }
-})
-
-/**this vue instance is for the keyboard events */
-// new Vue({
-//     el: "#vue-app",
-//     data : {
-//         name : "name",
-//         age : "age"
-//     },
-//     methods: {
-//         logName : function () {
-            
-//             console.log("you entered your name");
-//         },
-
-//         logAge : function () {
-//             console.log("you entered your age");
-//         }
-//     }
-
-
-// })
-
-
-
-
-/**this vue instance is for the  events lesson*/
-// new Vue({
-//     el: '#vue-app',
-//     data: {
-//         person : {
-//             name : "shaun"
-//     },
-//     age : 26,
-//         x : 0,
-//         y : 0
-//     },
-//     methods: {
-//         add : function (inc)    {
-//             this.age += inc;
-//         },
-//         subtract : function (dec) {
-//             this.age -= dec;
-//         },
-//         updateXY : function (event) {
-//            this.x = event.offsetX;
-//            this.y = event.offsetY;
-//         }
-//     }
-// });
+});
