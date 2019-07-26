a53d7859157ddca37c3e2a8c566fc53669728b95
codefingers <udohezekiel34@yahoo.com>
2019-07-26 09:16:51 +0100

    demod the component

---
+++ vueapp/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <h1>{{ title }}</h1>
+
+      {{greeting()}}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    greeting : function () {
+      return "hey man, you are almost there, keep pushing";
+    }
+  },
+}
+</script>
