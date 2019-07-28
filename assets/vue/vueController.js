352af67ee9e4be77286fbc3e03e1e4250da61e58
codefingers <udohezekiel34@yahoo.com>
2019-07-28 15:03:50 +0100

    tying out v-for

--- assets/vue/vueController.js
+++ assets/vue/vueController.js
@@ -1,23 +1,13 @@
 new Vue ({
     el : "#vue-app",
     data : {
-        width: 100,
-        ended: false
+        ninjas : [
+            {name: "boy", age: 25},
+            {name : "girl", age: 12}
+        ]
     },
     methods: {
-        punch : function () {
-            this.width -= 10;//this reduces the width of the bag health
-                            //by 10 everytime the function is fired
-
-            if (this.width <= 0) {//this changes the boolean value when the width has been completely reduced to zero
-                this.ended = true;
-            }
-
-        },
-        restart : function () {//this function restarts the game by changing the ended boolean value to true
-            this.ended = false;
-            this.width = 100;
-        }
+       
     },
     computed: {
 
