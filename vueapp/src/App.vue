25977d3fc8b96b6ab6ed78d593fd7fd53818ec70
codefingers <udohezekiel34@yahoo.com>
2019-08-07 15:25:54 +0100

    learnt what slotsare about and how to apply them

--- vueapp/src/App.vue
+++ vueapp/src/App.vue
@@ -1,40 +1,37 @@
 <template>
   <div>
-      <app-header v-bind:title="title" v-on:changeTitle="updateTitle($event)"></app-header>
-      <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
-      <app-footer v-bind:title="title"></app-footer>
+      <form-helper>
+            <div slot="form-header">
+              <h3>This is the title of the form</h3>
+              <p>information about the form</p>
+            </div>
+
+            <div slot="form-fields">
+              <input type="text" placeholder="name" required/>
+              <input type="password" placeholder="Password" required/>
+            </div>
+
+            <div slot="form-controls">
+                <button v-on:click="handleSubmit">Submit</button>
+            </div>
+      </form-helper>
   </div>
 </template>
 
 <script>
-import header from "./components/header.vue";
-import footer from "./components/footer";
-import ninja from "./components/ninja.vue";
+import formHelper from "./components/formHelper.vue";
 
 export default {
-    components : {
-      "app-header" : header,
-      "app-footer" : footer,
-      "app-ninjas" : ninja
+    components : { 
+           "form-helper" : formHelper
   },
   data() {
-     return {
-       ninjas : [
-              {name: 'Ryu', speciality: "Vue Components", show: false},
-              {name: 'Crystal', speciality: "HTML Wizardry", show: false},
-              {name: 'Hitoshi', speciality: "Click Events", show: false},
-              {name: 'Tango', speciality: "Conditionals", show: false},
-              {name: 'Kami', speciality: "Webpack", show: false},
-              {name: 'Yoshi', speciality: "Data Digging", show: false}
-          ],
-          title : "Vue Ninjas"
-     }
-  },
-  methods: {
-    updateTitle : function (newTitle) {
-      this.title = newTitle;
+    return {
+      title : "I am a dynamic title"
     }
-  },
+    
+  }
+  
 }
 </script>
 
