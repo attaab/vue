e4234a5c83aba10c9fce4c963a4610b93846233a
codefingers <udohezekiel34@yahoo.com>
2019-08-09 04:37:17 +0100

    added vue-router package and created instances for it

--- vueapp/src/main.js
+++ vueapp/src/main.js
@@ -1,11 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+import Routes from './routes'
 
 export const bus = new Vue();//creating an event Bus
 
 
 Vue.use(VueResource);//telling vue to use the resource file we imported
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+   routes : Routes
+   //the alternative of doing this is to register the routes as an array here and pssing the paths and components
+});
 
 //custom directives
 //vue diretive takes a couple of arguments
@@ -47,6 +55,7 @@ Vue.filter("snippet", function (value) {//filter function
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router: router //this object is teh router object we just created to route out our app
 });
 
---
+++ vueapp/src/routes.js
@@ -0,0 +1,14 @@
+//imports
+import showBlogs from "./components/showBlog.vue"
+import addBlog from "./components/addBlog.vue"
+
+export default [
+    {
+        path: "/",
+        component: showBlogs
+    },
+    {
+        path : "/add",
+        component: addBlog
+    }
+]
